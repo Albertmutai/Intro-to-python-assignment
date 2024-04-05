67948b3221bb2c4382c0a78458390238e7bd134f
Albertmutai <55190986+Albertmutai@users.noreply.github.com>
2024-04-05 13:07:30 +0300

    Add files via upload

--- user_input.py
+++ user_input.py
@@ -1,8 +1,4 @@
-Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: F:/Albert/PLP SOFTWARE DEV/user_input.py
-What is your name?:  Albert
-How old are you?: 38
- Where do you live Eldoret
-Hello Albert  you are 38 years old and live in  Eldoret
+name=str(input('What is your name?:  '))
+age=int(input('How old are you?: '))
+location=str(input(' Where do you live?:'))
+print('Hello' , name, ' you are',age, 'years old and live in',location)
